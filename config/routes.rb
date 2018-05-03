fefda4b09d9b33de648ca5662e5836621936cb2a
ampueropierre <ampuero.pierre@gmail.com>
2018-05-03 11:06:01 +0200

    Create controller and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'event/new'
+  get 'event/create'
+  get 'event/show'
+  get 'user/new'
+  get 'user/create'
+  get 'user/show'
+  get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    description "MyText"
+    date "2018-05-03 10:01:03"
+    place "MyString"
+  end
+end
